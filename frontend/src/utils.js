d05587c9b6346f4dc480649a6ba13fb1fa423a9e
Vishakha  Rawat <vishakhat895@gmail.com>
2025-04-27 23:36:00 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,4 +4,4 @@ export const notify = (message, type) => {
     toast[type](message);
 }
 
-export const API_URL = 'https://mern-task-manager-app-deploy-api.vercel.app';
+export const API_URL = 'https://mern-task-manager-app-api.vercel.app';
