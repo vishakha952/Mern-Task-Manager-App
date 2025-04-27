393abc1141b1f5d1ec433e1323cdd46a6888378e
Vishakha Thakur <vishakhat895@gmail.com>
2025-04-27 20:00:28 +0530

    second-commit

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-require('./Models/Db');
+require('./Models/db');
 const PORT = process.env.PORT || 8080;
 const TaskRouter = require('./Routes/TaskRouter');
 const bodyParser = require('body-parser');
