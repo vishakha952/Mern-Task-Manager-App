bb13794a56fceee5909d34d9ff06dd542c771a3a
Vishakha  Rawat <vishakhat895@gmail.com>
2025-04-28 12:22:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-require('./Models/db');
+require('./Models/Db');
 const PORT = process.env.PORT || 8080;
 const TaskRouter = require('./Routes/TaskRouter');
 const bodyParser = require('body-parser');
