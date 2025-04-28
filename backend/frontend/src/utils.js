87cc4819c1e4f7432d55134cacf19f75c39de2b3
Vishakha Thakur <vishakhat895@gmail.com>
2025-04-28 19:32:31 +0530

    Remove frontend folder from backend

--- backend/frontend/src/utils.js
+++
@@ -1,7 +0,0 @@
-import { toast } from 'react-toastify';
-
-export const notify = (message, type) => {
-    toast[type](message);
-}
-
-export const API_URL = 'https://mern-task-manager-app-api.vercel.app';
